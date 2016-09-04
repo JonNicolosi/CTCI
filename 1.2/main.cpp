06437bab7b14deb6aa59bb8cc3e76d00c5a00da1
Jon Nicolosi <jonicolosi@gmail.com>
2016-09-04 16:48:24 -0400

    Begun work on 1.2

---
+++ 1.2/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+bool isPermutation(string word1, string word2){
+
+
+
+
+}
+
+
+int main()
+{
+    string word1 = "hello";
+    string word2 = "olleh";
+}
