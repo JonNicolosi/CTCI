e27d08487eec6d4898ad5621bc1c66233dc7a9e6
Jon Nicolosi <jonicolosi@gmail.com>
2016-09-04 21:09:19 -0400

    Bulk of logic established

--- 1.2/main.cpp
+++ 1.2/main.cpp
@@ -4,6 +4,32 @@ using namespace std;
 
 bool isPermutation(string word1, string word2){
 
+    int arr[26];
+
+    for (int k = 0; k < 26; k++)
+        arr[k] = 0;
+
+    for(int i = 0; i < word1.length(); i++){
+
+        if(isupper(word1[i])==false){
+
+            arr[word1[i]-97]++;
+        }
+        else{
+            arr[word1[i]-65]++;
+        }
+    }
+
+    for(int k = 0; k < word2.length(); k++){
+
+        if(isupper(word2[k])==false){
+
+            arr[word2[k]-97]++;
+        }
+        else{
+            arr[word2[k]-65]++;
+        }
+    }
 
 
 
